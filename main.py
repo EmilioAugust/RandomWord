3f305009c0ebb9a2b3b95002df9200e4d419fdbd
Emil <kuluev.emil.8@gmail.com>
2025-12-24 09:17:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ from random_word import router
 
 app = FastAPI()
 app.include_router(router)
-favicon_path = 'favicon/favicon.ico'
 
 @app.get('/')
 async def main():
     return {'message': "Welcome!"}
-
-@app.get('/favicon.ico', include_in_schema=False)
-async def favicon():
-    return FileResponse(favicon_path)
